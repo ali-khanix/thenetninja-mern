89d1f7eca373e77e03af49d9ec2eeedd5ab29011
ali_khanix <khaniali568@gmail.com>
2024-07-17 18:41:05 +0330

    controller pt2

--- server/src/routes/routes.workouts.js
+++ server/src/routes/routes.workouts.js
@@ -1,37 +1,20 @@
 const express = require('express');
-const Workout = require('../models/model.workout.js');
+const {
+  createWorkout,
+  getWorkouts,
+  getWorkout,
+} = require('../controllers/controller.workout.js');
 
 const router = express.Router();
 
 // Get all workouts
-router.get('/', (req, res) => {
-  res.json({
-    message: 'Get All Workouts',
-  });
-});
+router.get('/', getWorkouts);
 
 // Get a single workout
-router.get('/:id', (req, res) => {
-  res.json({
-    message: 'GET a single workout',
-  });
-});
+router.get('/:id', getWorkout);
 
 // POST a new workout
-router.post('/', async (req, res) => {
-  const { title, load, reps } = req.body;
-
-  try {
-    const workout = await Workout.create({
-      title,
-      load,
-      reps,
-    });
-    res.status(200).json(workout);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post('/', createWorkout);
 
 // DELETE a workout
 router.delete('/:id', (req, res) => {
